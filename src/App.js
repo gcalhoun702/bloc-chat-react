440bb7ea475bb540434489b88994ba7af47fc083
Gregg Calhoun <greggcalhoun@Gregs-MacBook-Air.local>
2019-02-20 09:52:54 -0800

    working on listing chat rooms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC2AyYkZFAMHpF3hm9ifsMDzJEUGgmwwjE",
+    authDomain: "bloc-chat-c460d.firebaseapp.com",
+    databaseURL: "https://bloc-chat-c460d.firebaseio.com",
+    projectId: "bloc-chat-c460d",
+    storageBucket: "bloc-chat-c460d.appspot.com",
+    messagingSenderId: "828708485035"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <RoomList firebase={firebase} />
         </header>
       </div>
     );
